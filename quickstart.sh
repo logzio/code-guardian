f865392ddd3c2dd13415535e21180657b5c37e3a
yyyogev <yogev.metzuyanim@logz.io>
2020-01-22 18:49:33 +0200

    prints

--- quickstart.sh
+++ quickstart.sh
@@ -1,4 +1,5 @@
-echo "Copying neccesary files..."
+#!/usr/bin/env bash
+echo "Copying necessary files..."
 curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/pre-commit >> .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py >> .git/hooks/guardian-script.py
