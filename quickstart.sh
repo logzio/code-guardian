21ea9eab27f07be2eb6a0748d2db89f97ee01340
Roni.sh <roni.shaham@logz.io>
2020-01-22 21:44:03 +0200

    Install pathlib

--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Copying necessary files..."
+sudo easy_install pip
 curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/pre-commit >> .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py > .git/hooks/guardian-script.py
