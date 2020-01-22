e4707de48f6cef0acad5bcbd35a7d2daedc40316
Roni Shaham <ronish31@gmail.com>
2020-01-22 18:56:17 +0200

    Roni (#6)
    * Delete regex.py
    
    * Clean guardian
    
    * Clean guardian

--- guardian-script.py
+++ guardian-script.py
@@ -6,7 +6,6 @@ from pathlib import Path
 
 upper_and_lower_types = {"logzio shipping token"}
 rootpath_arr = sys.argv[1:len(sys.argv)]
-regex = "script"
 
 
 def walk_in_directory():
@@ -21,25 +20,25 @@ def walk_in_directory():
 
 
 def check_file_data(file_path):
-    # try:
+    try:
         with open(file_path) as fp:
             line = fp.readline()
             cnt = 1
             while line:
-                find_regex_mock(file_path, line, cnt)
+                search_regex(file_path, line, cnt)
                 line = fp.readline()
                 cnt += 1
-    # except:
-    #     pass
+    except:
+        pass
 
 
-def find_regex_mock(path, line, cnt):
+def search_regex(path, line, cnt):
     sensitive_data_arr = is_sensitive(line)
     for sensitive_data in sensitive_data_arr:
         dir = Path(os.path.abspath(__file__))
         fullpath = str(dir.parent)[:-11] + "/" + path
         logging.error("Warning! Line: {} may contain sensitive data:\n{}:{}\n{}\n".format(cnt, fullpath, cnt, sensitive_data.strip()))
-        print("sdf")
+        print("Found sensitive data\n")
 
 
 def is_sensitive(line_to_check):
@@ -52,33 +51,22 @@ def is_sensitive(line_to_check):
 
 
 def _get_sensitive_data_dictionary():
-    logzio_shipping_token_regex = "([a-zA-Z]{32})"
+    logzio_shipping_token_regex = r"\b[a-zA-Z]{32}\b"
+    logzio_api_token_regex = "([a-z]|[0-9]){8}[-]{1}([a-z]|[0-9]){4}[-]{1}([a-z]|[0-9]){4}[-]{1}([a-z]|[0-9]){4}[-]{1}([a-z]|[0-9]){11}"
     regex_list = {"logzio shipping token": logzio_shipping_token_regex}
+    regex_list["logzio api token"] = logzio_api_token_regex
     return regex_list
 
 
 def _is_sensitive_by_regex(regex, line_to_check, type_of_data):
     is_found_token = False
     match_obj = re.search(regex, line_to_check)
-    if match_obj is not None:
-        if match_obj.start() == 0 and match_obj.end() == len(line_to_check):
-            is_found_token = True
+    if match_obj is not None and match_obj.group() is not None:
+        if type_of_data in upper_and_lower_types:
+            if _is_lower_and_upper_string(match_obj.group()):
+                is_found_token = True
         else:
-            before_index = match_obj.start() - 1
-            after_index = match_obj.end()
-            if match_obj.start() == 0:
-                if (not line_to_check[after_index].islower()) and (not line_to_check[after_index].isupper()):
-                    is_found_token = True
-            if match_obj.end() == len(line_to_check):
-                if (not line_to_check[before_index].islower()) and (not line_to_check[before_index].isupper()):
-                    is_found_token = True
-            else:
-                if (not line_to_check[before_index].islower()) and (not line_to_check[before_index].isupper()) and\
-                        (not line_to_check[after_index].islower()) and (not line_to_check[after_index].isupper()):
-                    is_found_token = True
-        if is_found_token and type_of_data in upper_and_lower_types:
-            if not _is_lower_and_upper_string(match_obj.group()):
-                is_found_token = False
+            is_found_token = True
     return is_found_token
 
 
