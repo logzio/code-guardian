44a6816f2958c102541ef71de4d2a7d8de9f18eb
Yogev Mets <yogev.metzuyanim@logz.io>
2020-01-22 19:56:12 +0200

    Install pathlib (#8)
    * fixes
    
    * install pathlib and  remove prints

--- quickstart.sh
+++ quickstart.sh
@@ -2,6 +2,6 @@
 echo "Copying necessary files..."
 curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/pre-commit >> .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py >> .git/hooks/guardian-script.py
-
+curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py > .git/hooks/guardian-script.py
+pip install pathlib
 echo "Everything is ready, you can now code safely!"
