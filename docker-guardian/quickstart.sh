f3e8e8f740e0172c7e22473a26cb098b46e314b9
yyyogev <yogev.metzuyanim@logz.io>
2020-01-23 11:55:38 +0200

    Docker func

--- docker-guardian/quickstart.sh
+++ docker-guardian/quickstart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Copying necessary files..."
 mkdir ~/.docker-guard
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py > ~/.docker-guard/guardian-script.py
+curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/docker-guardian/guardian-script.py > ~/.docker-guard/guardian-script.py
+curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/docker-guardian/docker-func >> ~/.zshrc
+source ~/.zshrc
 echo "Everything is ready, you can now code safely!"
--- docker-plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir ~/.docker-guard
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py > ~/.docker-guard/guardian-script.py
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/docker-func >> ~/.zshrc
-pip install pathlib
