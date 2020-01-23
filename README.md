b19d041b72a5f0aed237be956160408295707bc8
Roni.sh <roni.shaham@logz.io>
2020-01-23 11:25:03 +0200

    docker readme updated

--- README.md
+++
@@ -1,6 +0,0 @@
-# code-guardian
-Run
-```
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/quickstart.sh | bash -s
-```
-That's it!
--- quickstart.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Copying necessary files..."
-sudo easy_install pip
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/pre-commit >> .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py > .git/hooks/guardian-script.py
-pip install pathlib
-echo "Everything is ready, you can now code safely!"
