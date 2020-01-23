9cf78b054c511df86c135c4144d092db303a4d9a
yyyogev <yogev.metzuyanim@logz.io>
2020-01-23 12:36:12 +0200

    remove source

--- docker-guardian/quickstart.sh
+++ docker-guardian/quickstart.sh
@@ -3,5 +3,4 @@ echo "Copying necessary files..."
 mkdir ~/.docker-guard
 curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/docker-guardian/guardian-script.py > ~/.docker-guard/guardian-script.py
 curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/docker-guardian/docker-func >> ~/.zshrc
-source ~/.zshrc
-echo "Everything is ready, you can now code safely!"
+echo "Everything is ready, you can now write dockers safely!"
