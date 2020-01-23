2899306b6ea93479a11041789e6cb86fa844c6cd
Yogev Mets <yogev.metzuyanim@logz.io>
2020-01-23 12:36:44 +0200

    Docker guardian func (#11)
    * Install pathlib
    
    * Git and Docker guardian scripts
    
    * Delete guardian-script.py
    
    * Delete pre-commit
    
    * Delete quickstart.sh
    
    * docker readme updated
    
    * add docker readme
    
    * Add docker guardian
    
    * Docker func
    
    * remove source
    
    Co-authored-by: Roni Shaham <ronish31@gmail.com>

--- docker-guardian/quickstart.sh
+++ docker-guardian/quickstart.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Copying necessary files..."
 mkdir ~/.docker-guard
-curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/guardian-script.py > ~/.docker-guard/guardian-script.py
-echo "Everything is ready, you can now code safely!"
+curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/docker-guardian/guardian-script.py > ~/.docker-guard/guardian-script.py
+curl -sSL https://raw.githubusercontent.com/logzio/code-guardian/master/docker-guardian/docker-func >> ~/.zshrc
+echo "Everything is ready, you can now write dockers safely!"
